9d1391144535c98896a4ed629bcce95f089a55df
Jeffrey Wilcke <stygeo@gmail.com>
2014-01-13 16:27:17 +0100

    Formatting

--- address.go
+++ address.go
@@ -1,33 +1,32 @@
 package mscutil
 
 import (
-  "github.com/conformal/btcutil"
-  "github.com/conformal/btcwire"
-  "github.com/conformal/btcscript"
+	"github.com/conformal/btcscript"
+	"github.com/conformal/btcutil"
+	"github.com/conformal/btcwire"
 )
 
 const ExodusAddress string = "1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P"
 
 func GetExodusTransactions(block *btcutil.Block) []*btcutil.Tx {
-  var txs []*btcutil.Tx
+	var txs []*btcutil.Tx
 
-  for _, tx := range txs {
-    mtx := tx.MsgTx()
-    for _, txOut := range mtx.TxOut {
-      // Extract the address from the script pub key
-      _, addrs, _, _ := btcscript.ExtractPkScriptAddrs(txOut.PkScript, btcwire.MainNet)
-      // Check each output address and if there's an address going to the exodus address
-      // we add it to tx slice
-      for _, addr := range addrs {
-        if addr.EncodeAddress() == ExodusAddress {
-          txs = append(txs, tx)
-          // Continue, we don't care if there are more exodus addresses
-          continue
-        }
-      }
-    }
-  }
+	for _, tx := range txs {
+		mtx := tx.MsgTx()
+		for _, txOut := range mtx.TxOut {
+			// Extract the address from the script pub key
+			_, addrs, _, _ := btcscript.ExtractPkScriptAddrs(txOut.PkScript, btcwire.MainNet)
+			// Check each output address and if there's an address going to the exodus address
+			// we add it to tx slice
+			for _, addr := range addrs {
+				if addr.EncodeAddress() == ExodusAddress {
+					txs = append(txs, tx)
+					// Continue, we don't care if there are more exodus addresses
+					continue
+				}
+			}
+		}
+	}
 
-  return txs
+	return txs
 }
-
